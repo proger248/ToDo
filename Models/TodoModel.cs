6e00cb3a5426414dfcef2c08f77df0b8ab1f2e97
VadikCherepanov <proger248@yandex.ru>
2022-03-30 13:32:24 +0300

    Initialized commit

---
+++ Models/TodoModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Windows.Models
+{
+    class TodoModel: INotifyPropertyChanged
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        private bool _isDone;
+        private string _text;
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set
+            {
+                if (IsDone == value) return;
+                _isDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+        public string Text
+        {
+            get { return _text; }
+            set {
+                if (_text == value) return;
+                _text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
